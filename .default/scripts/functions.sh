75b0f3161f230daa880b55d94a089db57f780e29
Igor Goryachev <igor@tegia.ru>
2024-09-14 13:54:01 +0300

    Добавлена установка справочников из внешнего репозитория

--- .default/scripts/functions.sh
+++ .default/scripts/functions.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 source ./const.sh
-source ./params.sh 2>/dev/null
-
+source ./params.sh
 
 # ////////////////////////////////////////////////////////////////////////////////////////
 #
@@ -40,6 +39,44 @@ mysql_debian_password()
     echo $passwd
 }
 
+# ////////////////////////////////////////////////////////////////////////////////////////
+#
+# INSTALL DATA
+#
+# ////////////////////////////////////////////////////////////////////////////////////////
+
+#
+# $2 - data nema
+# $1 - repository json info
+#
+
+tegia_data_install()
+{
+	local NAME=${1}
+	local REPOSITORY=${2}
+
+	#
+	# CHECK INSTALL
+	#
+	
+	if [ -d  ${ROOT_PATH}/data/${NAME}/ ] 
+	then
+		echo "${_OK_}data '${YELLOW}${NAME}${RESET}' is already installed"
+		return 0
+	fi
+
+	#echo "${REPOSITORY}"
+
+	URL=$(echo "${REPOSITORY}" | jq -r '.repository.url')
+	BRAHCH=$(echo "${REPOSITORY}" | jq -r '.repository.branch')
+
+	cd ${ROOT_PATH}/data/
+	git clone ${URL} ${NAME} -b ${BRAHCH}
+
+	echo "${_OK_}data '${YELLOW}${NAME}${RESET}' clone & checkout"
+	return 0
+}
+
 
 # ////////////////////////////////////////////////////////////////////////////////////////
 #
@@ -51,53 +88,57 @@ mysql_debian_password()
 # $1 - configuration name
 # $2 - github url
 # $3 - branch name
+# $4 - params json
 #
 
 tegia_conf_install()
 {
-	echo " "
-	echo "${YELLOW}CONFIGURATION ${1}${RESET}"
-	echo " "
-
-	CONF_NAME=$1
-	CONF_GIT_URL=$2
-	CONF_BRANCH_NAME=$3
-	CONF_CATALOG="$1@$3"
+	local CONF_NAME=$1
+	local CONF_GIT_URL=$2
+	local CONF_BRANCH_NAME=$3
+	local CONF_CATALOG="$1@$3"
+	local PARAMS=$4
 
 	#
 	# CHECK INSTALL
 	#
 	
-	if [ -d  ${root_folder}/configurations/$CONF_CATALOG/ ] 
+	if [ -d  ${ROOT_PATH}/configurations/$CONF_CATALOG/ ] 
 	then
-		echo "${_OK_}configuration for '$CONF_CATALOG' is already installed"
+		echo "${_OK_}configuration '${YELLOW}${CONF_CATALOG}${RESET}' is already installed"
 		return 0
 	fi
+
+	echo " "
+	echo "------------------------------------------------------------"
+	echo "INIT CONFIGURATION: ${GREEN}${CONF_CATALOG}${RESET}"
+	echo "------------------------------------------------------------"
+	echo " "
 
 	#
 	# SOURSE CODE
 	#
 
-	cd ${root_folder}/configurations/
-	git clone $CONF_GIT_URL $CONF_CATALOG
+	cd ${ROOT_PATH}/configurations/
+	git clone ${CONF_GIT_URL} ${CONF_CATALOG} -b ${CONF_BRANCH_NAME}
 
-	cd ${root_folder}/configurations/$CONF_CATALOG/
-	git checkout $CONF_BRANCH_NAME
+	#cd ${ROOT_PATH}/configurations/$CONF_CATALOG/
+	#git checkout $CONF_BRANCH_NAME
 
-	echo "${_OK_}source code '$CONF_NAME' clone & checkout"
+	echo "${_OK_}configuration '${YELLOW}${CONF_NAME}${RESET}' clone & checkout"
 
 	#
 	# DATABASE
 	#
 
-	for file in ${root_folder}/configurations/${CONF_CATALOG}/sql/install/*.sql
+	for file in ${ROOT_PATH}/configurations/${CONF_CATALOG}/sql/install/*.sql
 	do
 		if [ -f "$file" ]
 		then
 			echo "      [apply] $file"
 			cp "$file" "$file"_tmp
 			sed -i -e "s|{DB_PREFIX}|$MYSQL_DB_PREFIX|g" "$file"_tmp
-			mysql --defaults-extra-file=${tep_folder}/configurations/mysql.cnf < "$file"_tmp
+			mysql --defaults-extra-file=${CLUSTER_PATH}/configurations/mysql.cnf < "$file"_tmp
 			rm "$file"_tmp
 		fi
 	done
@@ -108,28 +149,20 @@ tegia_conf_install()
 	# CONFIG
 	#
 
-	cp "${tep_folder}/.default/configurations/${CONF_NAME}.json" "${tep_folder}/configurations/$CONF_NAME.json"
-	
-	sed -i -e "s|{PATH_TO_CONFIG}|${root_folder}/configurations/$CONF_CATALOG|g" "${tep_folder}/configurations/$CONF_NAME.json"
-	
-	sed -i -e "s|{MYSQL_DB_PREFIX}|$MYSQL_DB_PREFIX|g" "${tep_folder}/configurations/$CONF_NAME.json"
-	sed -i -e "s|{MYSQL_HOST}|$MYSQL_HOST|g" "${tep_folder}/configurations/$CONF_NAME.json"
-	sed -i -e "s|{MYSQL_PORT}|$MYSQL_PORT|g" "${tep_folder}/configurations/$CONF_NAME.json"
-	sed -i -e "s|{MYSQL_USER}|$MYSQL_USER|g" "${tep_folder}/configurations/$CONF_NAME.json"
-	sed -i -e "s|{MYSQL_PASSWORD}|$MYSQL_PASSWORD|g" "${tep_folder}/configurations/$CONF_NAME.json" 
+	local CONFIG=$(init_config_file "${CLUSTER_PATH}/.default/configurations/${CONF_NAME}.json" "${PARAMS}")
 
-	sed -i -e "s|{TEGIA_PORT}|$TEGIA_FCGI_PORT|g" "${tep_folder}/configurations/$CONF_NAME.json" 
-	sed -i -e "s|{TEGIA_HOST}|$TEGIA_HOST|g" "${tep_folder}/configurations/$CONF_NAME.json" 
-	sed -i -e "s|{TEGIA_APP}|$TEGIA_APP|g" "${tep_folder}/configurations/$CONF_NAME.json" 
-
-	echo -e "${_OK_}create './configurations/$CONF_NAME.json'"
+	# echo "${CONFIG}"
+	echo "${CONFIG}" > "${CLUSTER_PATH}/configurations/${CONF_NAME}.json"
 
 	#
 	# INSTAAL
 	#
 
-	cd ${root_folder}/configurations/$CONF_CATALOG/bin
+	cd ${ROOT_PATH}/configurations/${CONF_CATALOG}/bin
 	bash ./install.sh
 
-	echo "${_OK_}configuration '$CONF_NAME' installed successfull"
+	echo " "
+	echo "${_OK_}configuration '${YELLOW}${CONF_CATALOG}${RESET}' installed successfull"
+
+	return 0
 }
